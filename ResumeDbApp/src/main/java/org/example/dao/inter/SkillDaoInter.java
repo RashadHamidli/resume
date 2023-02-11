29cc3b4422d02c4dec826674782fa8e8f01e1418
RashadHamidli <mr_rashad@email.com>
2023-02-11 18:47:56 +0400

    add

--- ResumeDbApp/src/main/java/org/example/dao/inter/SkillDaoInter.java
+++
@@ -1,9 +0,0 @@
-package org.example.dao.inter;
-
-import org.example.entity.Skill;
-
-import java.util.List;
-
-public interface SkillDaoInter {
-    public List<Skill> getAll();
-}
--- ResumeDbApp/src/main/java/org/example/dao/inter/UserSkillDaoInter.java
+++
@@ -1,9 +0,0 @@
-package org.example.dao.inter;
-
-import org.example.entity.UserSkill;
-
-import java.util.List;
-
-public interface UserSkillDaoInter {
-    public List<UserSkill> getAllSkillByUserId(int userId);
-}
