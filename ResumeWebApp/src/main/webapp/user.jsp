ea0fa281fadd38dcb80ea88991d2ba8a79aa5484
RashadHamidli <mr_rashad@email.com>
2023-02-26 15:01:15 +0400

    add web servlet

---
+++ ResumeWebApp/src/main/webapp/user.jsp
@@ -0,0 +1,33 @@
+<%@ page import="org.example.dao.inter.UserDaoInter" %>
+<%@ page import="org.example.main.Context" %>
+<%@ page import="org.example.entity.User" %><%--
+  Created by IntelliJ IDEA.
+  User: mr_rashad
+  Date: 25.02.2023
+  Time: 16:38
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>JSP page</title>
+</head>
+<body>
+<%
+    UserDaoInter userDao = Context.instanceUserDao();
+    User u = userDao.getById(8);
+%>
+<div>
+    <form action="UserController" method="POST">
+        <input type="hidden" name="id" value="<%=u.getId()%>"/>
+    <label>name:</label>
+    <input type="text" name="name" value="<%=u.getName()%>"/>
+    <br/>
+    <label>surname:</label>
+    <input type="text" name="surname" value="<%=u.getSurname()%>"/>
+    <input type="submit" name="Save" value="Save" >
+    </form>
+</div>
+</body>
+</html>
---
+++ ResumeWebAppDB/src/main/java/org/company/resumewebappdb/UserController.java
@@ -0,0 +1,48 @@
+package org.company.resumewebappdb;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import org.example.dao.inter.UserDaoInter;
+import org.example.entity.User;
+import org.example.main.Context;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@WebServlet(name = "UserController", value = "/UserController")
+public class UserController extends HttpServlet {
+
+    private UserDaoInter userDao = Context.instanceUserDao();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.valueOf(request.getParameter("id"));
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        String address = request.getParameter("address");
+        String phone = request.getParameter("phone");
+        String email = request.getParameter("email");
+        
+
+        System.out.println("name=" + name);
+        System.out.println("surname=" + surname);
+        System.out.println("address=" + address);
+        System.out.println("phone=" + phone);
+        System.out.println("email=" + email);
+
+        User user = userDao.getById(id);
+        user.setName(name);
+        user.setSurname(surname);
+        user.setAddress(address);
+        user.setPhone(phone);
+        user.setEmail(email);
+        userDao.updateUser(user);
+        response.sendRedirect("user.jsp");
+
+    }
+}
