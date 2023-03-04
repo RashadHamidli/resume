0ff46824bf038d9c28e0a0c51f93996ff550f76f
RashadHamidli <mr_rashad@email.com>
2023-03-04 20:22:37 +0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	jpa/ResumeDbApp/target/classes/org/example/Main.class
    #	jpa/ResumeDbApp/target/classes/org/example/dao/imple/CountryDaoImpl.class
    #	jpa/ResumeDbApp/target/classes/org/example/dao/imple/EmploymentHistoryDaoImpl.class
    #	jpa/ResumeDbApp/target/classes/org/example/dao/imple/SkillDaoImpl.class
    #	jpa/ResumeDbApp/target/classes/org/example/dao/imple/UserDaoImpl.class
    #	jpa/ResumeDbApp/target/classes/org/example/dao/imple/UserSkillDaoImpl.class
    #	jpa/ResumeDbApp/target/classes/org/example/entity/Country.class
    #	jpa/ResumeDbApp/target/classes/org/example/entity/EmploymentHistory.class
    #	jpa/ResumeDbApp/target/classes/org/example/entity/Skill.class
    #	jpa/ResumeDbApp/target/classes/org/example/entity/User.class
    #	jpa/ResumeDbApp/target/classes/org/example/entity/UserSkill.class

--- jdbc/ResumeDbApp/src/main/java/org/example/Main.java
+++ jdbc/ResumeDbApp/src/main/java/org/example/Main.java
@@ -10,7 +10,7 @@ import org.example.main.Context;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        User u = new User(8, "Rashad", "Hamidli", "+994708288899", "mr_rashad@email.com", "new Java Application", null, null, null, "Baku, Azerbaijan");
+        User u = new User(0, "Rashad", "Hamidli", "+9942557794", "mr_rashad@email.com", "new Java Application", null, null, null, "Baku, Azerbaijan");
        u.setPassword("12345");
         new UserDaoImpl().addUser(u);
     }
